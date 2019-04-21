06575ea11fc943a08bf478780d901fdd49bed480
darknessinsideme <hasanu12240998@gmail.com>
2019-04-22 01:52:44 +0300

    I Add the TopNavBar

--- src/components/signInOrUp/login.js
+++ src/components/signInOrUp/login.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import TopNav from '../topNavigation/topNav'
 import './login.css'
 
 
 function Login(){
     return(
-        <div className="form-style-10">
-            <form>
-                <div className="section"><span>1</span>Login</div>
-                <div className="inner-wrap">
-                    <label>Email <input type="text" name="email" /></label>
-                    <label>Password <textarea name="password"></textarea></label>
-                </div>
-                <input style={{marginLeft:'35%' ,}}type="submit" name="login" value='Login'/><br/>
-                <p style={{marginLeft:'12%' ,}}>I don't have account <a href="/signup">create account</a></p>
-            </form> 
-        </div>   
+        <div>
+            <TopNav />
+            <div className="form-style-10">
+                
+                <form>
+                    <div className="section"><span>1</span>Login</div>
+                    <div className="inner-wrap">
+                        <label>Email <input type="text" name="email" /></label>
+                        <label>Password <textarea name="password"></textarea></label>
+                    </div>
+                    <input style={{marginLeft:'35%' ,}}type="submit" name="login" value='Login'/><br/>
+                    <p style={{marginLeft:'12%' ,}}>I don't have account <a href="/signup">create account</a></p>
+                </form> 
+            </div>
+        </div>  
     
     )
 }
--- src/components/signInOrUp/signUp.js
+++ src/components/signInOrUp/signUp.js
@@ -1,22 +1,25 @@
 import React from 'react';
-// import topNav from ''
+import TopNav from '../topNavigation/topNav'
 import './login.css'
 
 
 function SignUp(){
     return(
-        <div className="form-style-10">
-            <form>
-                <div className="section"><span>1</span>Sign Up</div>
-                <div className="inner-wrap">
-                    <label>Your name <input type="text" name="name" /></label>
-                    <label>Email <textarea name="email"></textarea></label>
-                    <label>Password <input type="text" name="password" /></label>
-                    <label>Confirm Password <textarea name="Cpassword"></textarea></label>
-                </div>
-                <input type="submit" name="Sign Up" value='Sign Up'/>
-            </form> 
-        </div>   
+        <div>
+            <TopNav />
+            <div className="form-style-10">
+                <form>
+                    <div className="section"><span>1</span>Sign Up</div>
+                    <div className="inner-wrap">
+                        <label>Your name <input type="text" name="name" /></label>
+                        <label>Email <textarea name="email"></textarea></label>
+                        <label>Password <input type="text" name="password" /></label>
+                        <label>Confirm Password <textarea name="Cpassword"></textarea></label>
+                    </div>
+                    <input type="submit" name="Sign Up" value='Sign Up'/>
+                </form> 
+            </div>
+        </div> 
     
     )
 }
